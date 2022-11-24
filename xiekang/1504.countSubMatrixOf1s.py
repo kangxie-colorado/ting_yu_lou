d552f16d3b026b3c620de21001da493c48d418cc
kangxie-colorado <arking1981@gmail.com>
2022-11-23 21:15:22 -0700

    Update 1504.countSubMatrixOf1s.py

--- xiekang/1504.countSubMatrixOf1s.py
+++ xiekang/1504.countSubMatrixOf1s.py
@@ -51,27 +51,28 @@ class Solution:
       
    
   # dp plus monotonic stack
-  class Solution:
+class Solution:
     def numSubmat(self, mat: List[List[int]]) -> int:
         m, n = len(mat), len(mat[0])
         res = 0
 
         for i in range(m):
-            rowStack = []  # (val,idx)
+            rowStack = [(0, -1, 0)]  # (val,idx, dpSum)
             for j in range(0, n):
                 if mat[i][j]:
+                    myContri = 0
                     # calculate the column pre-sums
                     if i > 0:
                         mat[i][j] = mat[i-1][j] + 1
                     idx = j
-                    res += mat[i][j]
+                    # anything bigger equal than me
                     while rowStack and rowStack[-1][0] >= mat[i][j]:
-                        _, _, idx = rowStack.pop()
-                    res += mat[i][j] * (j-idx)
+                        rowStack.pop()
+                    myContri += mat[i][j] * (j-rowStack[-1][1])
+                    myContri += rowStack[-1][2]
+                    rowStack.append((mat[i][j], j, myContri))
+                    res += myContri
 
-                    for val, origIdx, extendIdx in rowStack:
-                        res += val * (origIdx-extendIdx+1)
-                    rowStack.append((mat[i][j], j, idx))
                 else:
-                    rowStack = []
+                    rowStack.append((0, j, 0))
         return res
